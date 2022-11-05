0c9ddc2165e756656d5f46f311767b2e0bf5b626
anjalisaxena77 <85076238+anjalisaxena77@users.noreply.github.com>
2022-11-05 13:03:23 +0530

    Valid Palindrome in String

--- day 12/Palindrome in String.java
+++ day 12/Palindrome in String.java
@@ -1,4 +1,4 @@
-Question:
+/* Question:
 A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
 Given a string s, return true if it is a palindrome, or false otherwise.
 
@@ -23,7 +23,7 @@ Since an empty string reads the same forward and backward, it is a palindrome.
 Constraints:
 
 1 <= s.length <= 2 * 105
-s consists only of printable ASCII characters.
+s consists only of printable ASCII characters. */
 
 Solution:
 class Solution {

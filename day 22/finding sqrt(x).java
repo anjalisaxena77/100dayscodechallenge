3f4d6e7446ce39007c48650364e089e7e27e256a
anjalisaxena77 <85076238+anjalisaxena77@users.noreply.github.com>
2022-11-05 16:24:44 +0530

    finding sqrt(x)

---
+++ day 22/finding sqrt(x).java
@@ -0,0 +1,47 @@
+/* Question: 
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+You must not use any built-in exponent function or operator.
+
+For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+ 
+
+Example 1:
+
+Input: x = 4
+Output: 2
+Explanation: The square root of 4 is 2, so we return 2.
+Example 2:
+
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+ 
+
+Constraints:
+
+0 <= x <= 231 - 1
+
+
+
+*/
+Solution:
+class Solution {
+    public int mySqrt(int x) {
+        if(x <= 1) return x;
+        int start = 1;
+        int end = x/2;
+        
+        while(start < end) {
+            int mid = (start + (end-start)/2) + 1;
+            
+            int div = x/mid;
+            if(div == mid) return mid;
+            if(div > mid) start = mid;
+            else end = mid-1;
+        }
+        
+        return start;
+        
+    }
+}

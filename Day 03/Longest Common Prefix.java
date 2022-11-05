3160ebeb1014ebe145bca2f959b6f70a3d376c43
anjalisaxena77 <85076238+anjalisaxena77@users.noreply.github.com>
2022-11-05 12:19:00 +0530

    Longest common Prefix

--- Day 03/Longest Common Prefix.java
+++ Day 03/Longest Common Prefix.java
@@ -1,4 +1,4 @@
-Question: Write a function to find the longest common prefix string amongst an array of strings.
+/*Question: Write a function to find the longest common prefix string amongst an array of strings.
 
 If there is no common prefix, return an empty string "".
 
@@ -17,7 +17,7 @@ Explanation: There is no common prefix among the input strings.
 Constraints:
 1 <= strs.length <= 200
 0 <= strs[i].length <= 200
-strs[i] consists of only lowercase English letters.
+strs[i] consists of only lowercase English letters. */
 
 
 Solution: 

985fd3538f7b556dfc148e6c0c1e386a7ce5e524
anjalisaxena77 <85076238+anjalisaxena77@users.noreply.github.com>
2022-11-13 22:24:16 +0530

    Update Birthday Candles.java

--- day 45/Birthday Candles.java
+++ day 45/Birthday Candles.java
@@ -1,3 +1,22 @@
+/*Question: You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. 
+They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+
+Input Format
+
+The first line contains a single integer,n , the size of candles .
+The second line contains 'n' space-separated integers, where each integer 'i'  describes the height of candles[i].
+
+
+Sample Input 0
+
+4
+3 2 1 3
+Sample Output 0
+
+2
+
+*/
+Solution:
 import java.util.*;
 
 class Solution{

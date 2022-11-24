a88880183c3cb6576113e5208bff1f2f8a864f9f
anjalisaxena77 <85076238+anjalisaxena77@users.noreply.github.com>
2022-11-24 21:15:26 +0530

    Save the prisoner Problem

---
+++ day 54/Save the prisioner.java
@@ -0,0 +1,91 @@
+/* Question: 
+A jail has a number of prisoners and a number of treats to pass out to them. Their jailer decides the fairest way to divide the treats is to seat the prisoners around a circular table in sequentially numbered chairs. A chair number will be drawn from a hat. Beginning with the prisoner in that chair, one candy will be handed to each prisoner sequentially around the table until all have been distributed.
+
+The jailer is playing a little joke, though. The last piece of candy looks like all the others, but it tastes awful. Determine the chair number occupied by the prisoner who will receive that candy.
+
+Sample Input 0
+
+2
+5 2 1
+5 2 2
+Sample Output 0
+
+2
+3
+
+*/
+Solution:
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'saveThePrisoner' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER m
+     *  3. INTEGER s
+     */
+     
+
+   public static int saveThePrisoner(int n, int m, int s) {
+
+
+int startPos = s;
+int numCandy = m;
+int numPrisoners = n;
+
+int answer = (startPos + (numCandy % numPrisoners) - 1);
+
+if( answer > numPrisoners)
+    answer %= numPrisoners;
+if( answer == 0)
+   answer = numPrisoners;  
+
+ return answer;
+
+}
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int n = Integer.parseInt(firstMultipleInput[0]);
+
+                int m = Integer.parseInt(firstMultipleInput[1]);
+
+                int s = Integer.parseInt(firstMultipleInput[2]);
+
+                int result = Result.saveThePrisoner(n, m, s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}

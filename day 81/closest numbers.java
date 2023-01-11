a89ace7e5843014cf689b3a6a85a9741a7a36fcb
anjalisaxena77 <85076238+anjalisaxena77@users.noreply.github.com>
2023-01-11 21:35:58 +0530

    Closest Numbers Problem

--- day 81/closest numbers.java
+++ day 81/closest numbers.java
@@ -1,3 +1,18 @@
+/* Question: 
+Sorting is useful as the first step in many different tasks. The most common task is to make finding things easier, but there are other uses as well.
+In this case, it will make it easier to determine which pair
+or pairs of elements have the smallest absolute difference between them.
+
+Sample Input 0
+
+10
+-20 -3916237 -357920 -3620601 7374819 -7330761 30 6246457 -6461594 266854 
+Sample Output 0
+
+-20 30
+
+*/ 
+Solution:
 import java.io.*;
 import java.util.*;
 
